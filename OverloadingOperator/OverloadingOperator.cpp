eb033fda527904a814965b202fe5856f99fff425
MinhHieuDL <minhhieuhcmute@gmail.com>
2019-01-18 10:32:12 +0700

    add the function double operator + (Employee) in class Employee of project OverloadingOperation

--- OverloadingOperator/OverloadingOperator.cpp
+++ OverloadingOperator/OverloadingOperator.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 using namespace std;
+
 class Employee
 {
 private:
@@ -8,29 +9,40 @@ private:
 public:
 	Employee(int, double);
 	double addTwo(Employee);
+	double operator + (Employee);
 };
+
 Employee::Employee(int id, double sal)
 {
 	idNum = id;
 	salary = sal;
 }
+
 double Employee::addTwo(Employee emp)
 {
 	double total;
 	total = salary + emp.salary;
 	return total;
 }
+
+double Employee::operator+(Employee em)
+{
+	double sum = 0;
+	sum = salary + em.salary;
+	return sum;
+}
+
 int main()
 {
 	Employee clerk(1234, 400.00);
 	Employee driver(3456, 650.00);
 	double sum;
-	sum = clerk.addTwo(driver);
-	cout << "Adding driver to clerk - Sum is $" <<
-		sum << endl;
-	sum = driver.addTwo(clerk);
-	cout << "Adding clerk to driver - Sum is $" <<
-		sum << endl;
+	sum = clerk.operator+(driver);
+	cout << "Adding driver to clerk - Sum is $" << sum << endl;
+	sum = driver.operator+(clerk);
+	cout << "Adding clerk to driver - Sum is $" << sum << endl;
+
 	cin.get();
+
 	return 0;
 }
